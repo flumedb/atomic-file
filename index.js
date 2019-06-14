7035ca863ca4ed6f549b7b9513d7284982aa60a5
Dominic Tarr <dominic.tarr@gmail.com>
2019-06-14 07:12:15 +0200

    pass suffix!

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const Store = require('./store/fs')
 const Inject = require('./inject')
 
 module.exports = function (filename, suffix, codec) {
-  return Inject(Store(filename, codec && codec.buffer), codec)
+  return Inject(Store(filename, suffix, codec && codec.buffer), codec)
 }
